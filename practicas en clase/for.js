3aa7f53e64e90bae3b930468b4b3945ba6b3962f
Andres Arias <santo2828@gmail.com>
2025-05-09 12:26:02 -0500

    actualizacion clase de hoy

---
+++ practicas en clase/for.js
@@ -0,0 +1,26 @@
+//for(let i=1; i<=100; i=i+1){
+  //  console.log(i )
+//}
+
+
+let i=1
+while(i<=20){
+    console.log(i*2)
+    i=i+1
+}
+
+
+i=1
+do{
+    console.log(i*2)
+ i=i+1
+    
+}while(i<=20)
+
+    for(let i=1;i<=10;i++){
+        if(i%2===1){
+            console.log(i)
+        }else{
+            
+        }
+    }
